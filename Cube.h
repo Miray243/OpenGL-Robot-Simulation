fa3391e27f9d72272b2f90f1e80cd0fdcb918d21
Miray Balıkoğlu <145465438+Miray243@users.noreply.github.com>
2026-01-21 22:46:07 +0300

    Define Cube class with draw method
    Refactor Cube class definition and add draw method.

--- Cube.h
+++ Cube.h
@@ -1,9 +1,10 @@
-// Cube.cpp
-#include "Cube.h"
-#include <GL/glut.h>
+// Cube.h
+#ifndef CUBE_H
+#define CUBE_H
 
-void Cube::draw() {
-    // 1 birimlik standart bir küp çizer.
-    // Room.cpp içinde glScalef ile boyutlandırıldığı için bu yeterlidir.
-    glutSolidCube(1.0f); 
-}
+class Cube {
+public:
+    void draw();
+};
+
+#endif
